ecae6f71ac491061cbe416eb4fe375eb99cb7178
Anmol Singh <anmolshl2@gmail.com>
2018-09-21 20:00:36 -0700

    git init along with remote origin set

--- src/app/components/under.construction/under.construction.ts
+++ src/app/components/under.construction/under.construction.ts
@@ -7,10 +7,13 @@ import {AfterViewInit, Component, ElementRef} from '@angular/core';
 })
 export class UnderConstructionComponent implements AfterViewInit {
   title = 'Under Construction!';
+  underConstructionImage: string;
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef) {
+    this.underConstructionImage = './assets/under_construction.png';
+  }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#8E2AD2';
   }
 
